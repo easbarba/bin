8d53b1f4fd5a6669c20cef6f17df0a884ff09d85
easbarba <easbarba@outlook.com>
2023-02-25 18:10:10 -0300

    pack: move to gota

--- gota.py
+++ gota.py
@@ -13,6 +13,8 @@
 # You should have received a copy of the GNU General Public License
 # along with Packer. If not, see <https://www.gnu.org/licenses/>.
 
+# TODO: log commands
+
 import sys
 import os
 import json
@@ -22,10 +24,18 @@ json_file = sys.argv[1]
 with open(json_file) as json_data:
     data = json.load(json_data)
 
-for lang in data:
-    print("\n", lang["lang"], "\n")
+print("\n", data["lang"], "\n")
 
-    for key, value in lang["packages"].items():
-        for i in value:
-            print("\n", i, "\n")
-            os.system(f"{lang['command']} i")
+# TODO: check deps
+
+# install all packages
+for key, value in data["packages"].items():
+    for i in value:
+        print("\n", i, "\n")
+        os.system(f"{data['command']} i")
+
+# run post command
+if "post" in data.keys():
+    os.system(data['post'])
+else:
+    print("post command not provided")
