5cc20fe80d0fed1835f3fdb70596fc817157c632
easbarba <easbarba@outlook.com>
2023-08-22 01:16:44 -0300

    rust: wm-volume

---
+++ s-wm-volume.rs
@@ -0,0 +1,11 @@
+#!/usr/bin/env rust-script
+//! Dependencies can be specified in the script file itself as follows:
+//!
+//! ```cargo
+//! [dependencies]
+//! rand = "0.8.0"
+//! ```
+
+fn main() {
+    println!("Hello, {}", "World!");
+}
